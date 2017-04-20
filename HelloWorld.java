f3819e07c1617208aaf8d84ef875b79d91835125
raviteja4c3 <raviteja4c3@gmail.com>
2017-04-20 15:27:16 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package tpoint;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class HelloWorld {
+	
+	public void printMe(){
+		System.out.println("Message Printed!!!");
+	}
+
+
+}
